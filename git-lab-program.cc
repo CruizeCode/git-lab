1375704ededce8446333fc24cac7281c06ed9b02
Johnathan Bachtel <jb423216@ohio.edu>
2019-01-23 19:39:19 -0500

    @markamay

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *      File: git-lab-program.cc
+ *      Author: Johnathan Bachtel   
+ *      Date: 1/23/19
+ *      Description: C++ in git
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+     cout << "Hello git!!" <<endl ;
+    return 0;
+}// main
